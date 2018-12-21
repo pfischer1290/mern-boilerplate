03e1806500508c0ff1ed11e10727079fb05267de
Pascal Fischer <pf@ease.fit>
2018-12-21 14:41:41 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hi, i will be the boilerplate for .dotmode apps! :))</div>
+    );
+  }
+}
+
+export default App;
